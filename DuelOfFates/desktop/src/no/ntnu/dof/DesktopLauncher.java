43606d172ce850ea40ca2df08096a9e2a769a2ef
s0mark <somorjai.mark@icloud.com>
2024-03-15 17:59:03 +0100

    moved packages

--- DuelOfFates/desktop/src/no/ntnu/dof/DesktopLauncher.java
+++ DuelOfFates/desktop/src/no/ntnu/dof/DesktopLauncher.java
@@ -1,8 +1,8 @@
-package com.mygdx.game;
+package no.ntnu.dof;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.Game;
+import no.ntnu.dof.Game;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
