a6e25ceccb3f12eadaf4546006312f80370b1855
dion <dion.rigatos@gmail.com>
2024-04-04 00:36:42 +0200

    Add basic logging w/ slf4j-simple

--- DuelOfFates/desktop/src/no/ntnu/dof/DesktopLauncher.java
+++ DuelOfFates/desktop/src/no/ntnu/dof/DesktopLauncher.java
@@ -3,14 +3,18 @@ package no.ntnu.dof;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
+
+import lombok.extern.slf4j.Slf4j;
 import no.ntnu.dof.controller.DuelOfFates;
 import no.ntnu.dof.controller.network.ServiceLocator;
 import no.ntnu.dof.desktop.FirebaseAuthImpl;
 import no.ntnu.dof.desktop.FirebaseLobbyService;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+@Slf4j
 public class DesktopLauncher {
     public static void main(String[] arg) {
+        log.info("Starting Duel of Fates");
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setForegroundFPS(60);
         config.setTitle("Duel of Fates");
