4f488b84080019a061b735edeef8dc0fc3b4adc4
Foconchasseur <arosset@enssat.fr>
2024-04-12 13:28:38 +0200

    Add of endScreen method in GameScreen and display it

--- DuelOfFates/core/src/no/ntnu/dof/view/screens/game/GameScreen.java
+++ DuelOfFates/core/src/no/ntnu/dof/view/screens/game/GameScreen.java
@@ -2,10 +2,16 @@ package no.ntnu.dof.view.screens.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 
+import no.ntnu.dof.controller.gameplay.player.ClickHostPlayerController;
 import no.ntnu.dof.model.gameplay.Game;
+import no.ntnu.dof.view.entity.label.TextLabel;
 import no.ntnu.dof.view.entity.view.GameView;
 
 
@@ -13,9 +19,11 @@ public class GameScreen implements Screen {
     private Stage stage;
 
     private final GameView gameView;
+    private final Game game;
 
     public GameScreen(Game game) {
         this.gameView = new GameView(game);
+        this.game = game;
     }
 
     @Override
@@ -31,8 +39,13 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        stage.act(delta);
-        stage.draw();
+        if(!game.isOver()) {
+            stage.act(delta);
+            stage.draw();
+        } else {
+            EndScreen();
+        }
+
     }
 
     @Override
@@ -58,6 +71,14 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+    public void EndScreen() {
+        TextLabel endLabel = new TextLabel("WINNER", 0, 0,Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 1, Color.GREEN);
+        stage.addActor(endLabel.getText());
+        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+        TextButton ReturnToMenu = new TextButton("End Turn", skin, "default");
+        //ReturnToMenu.addListener();
     }
 
 
