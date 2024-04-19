723f15fba9a2dc5a987085719820a9807182f851
s0mark <somorjai.mark@icloud.com>
2024-04-19 11:46:27 +0200

    removed unused classes

--- DuelOfFates/core/src/no/ntnu/dof/model/gameplay/playerclass/PlayerClassStrategy.java
+++
@@ -1,8 +0,0 @@
-package no.ntnu.dof.model.gameplay.playerclass;
-
-import lombok.NonNull;
-import no.ntnu.dof.model.gameplay.player.Player;
-
-public interface PlayerClassStrategy {
-    void apply(@NonNull final Player player);
-}
