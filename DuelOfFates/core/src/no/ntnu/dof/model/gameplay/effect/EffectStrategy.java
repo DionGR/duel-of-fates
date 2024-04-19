2f0221d4d16f35ac347ae87dce14e84a2c071152
dion <dion.rigatos@gmail.com>
2024-04-19 12:44:11 +0200

    Undo "removed unnused classes"

---
+++ DuelOfFates/core/src/no/ntnu/dof/model/gameplay/effect/EffectStrategy.java
@@ -0,0 +1,8 @@
+package no.ntnu.dof.model.gameplay.effect;
+
+import lombok.NonNull;
+import no.ntnu.dof.model.gameplay.player.Player;
+
+public interface EffectStrategy {
+    void apply(@NonNull final Player player);
+}
